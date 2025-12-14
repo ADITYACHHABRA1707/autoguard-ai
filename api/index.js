c1326d9ee9db253d17038bbb9f173975c7c31530
Aditya Chhabra <adityachhabra2368@gmail.com>
2025-12-14 10:12:02 +0000

    fix: make express app vercel serverless compatible

--- api/index.js
+++ api/index.js
@@ -1,4 +1,6 @@
-import serverless from "serverless-http";
 import app from "../server.js";
 
-export default serverless(app);
+export default function handler(req, res) {
+  return app(req, res);
+}
+
